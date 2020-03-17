b5f608fe8c740616a6149fecf0cb7f67dbb05225
fd3siguenza <36047344+fd3siguenza@users.noreply.github.com>
2020-03-16 20:10:45 -0400

    Update testRabbitMQClient.php

--- project/API/testRabbitMQClient.php
+++ project/API/testRabbitMQClient.php
@@ -4,23 +4,13 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");//connect to MQ exchange
 
-
-//$request = array(); array is established
-//$request['type'] = "login";
-//$request['username'] = $argv[1];
-//$request['password'] = $argv[2];
-//$request['message'] = "HI";
 echo"A";//test echo
-//$response = $client->send_request($request);
-//$response = $client->publish($request); establishes connection to MQ
-echo "B"; //test echo
-//echo "client received response: ".PHP_EOL;
-//print_r($response);
-echo "\n\n";
 
-//echo $argv[0]." END".PHP_EOL;
+echo "B"; //test echo
+
+
 
 
 
@@ -54,7 +44,6 @@ if ($err) {
 	echo "cURL Error #:" . $err;
 } else {
 	echo $respons;
-//$respons = $client->send_request($curl);
-//$respons = $client->publish($curl);
+
 
 }
