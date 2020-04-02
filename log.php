f9babed7982fbcee2525ef3bef62fbec3955820b
Ubuntu <ubuntu@ip-10-0-0-89.us-east-2.compute.internal>
2020-04-02 09:11:31 +0000

    here

---
+++ log.php
@@ -0,0 +1,9 @@
+//error_log ( string $message [, int $message_type = 0 [, string $destination [, string $extra_headers ]]] ) : bool
+
+<?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+//echo copy("rabbit@ip-10-0-0-89.log","log.txt");
+?>
