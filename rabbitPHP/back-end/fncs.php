6d6aca66cb4c71f10b7a795cb9692f03b788bb1f
Ubuntu <ubuntu@ip-10-0-0-100.us-east-2.compute.internal>
2020-04-14 22:39:43 +0000

    added back-end file w/ sql dump

--- rabbitPHP/back-end/fncs.php
+++ rabbitPHP/back-end/fncs.php
@@ -1,4 +1,106 @@
-<?php
+<?php 
+
+
+
+
+
+function saved($u){
+
+$db = getDBconnect();
+$sql = "SELECT id FROM Accounts WHERE username='$u' ";
+$result = mysqli_query($db,$sql);
+$num = mysqli_num_rows($result);
+if ($num = 1){
+	$row = mysqli_fetch_row($result);
+	$id = $row[0];
+	$db = getDBconnect();
+	$query = "SELECT DrinkName, AlcType, Ingredients, Instructions from CustomRep WHERE id = '$id'";
+	$results = mysqli_query($db, $query);
+	$nums = mysqli_num_rows($results);
+	if ($nums > 0){
+		echo "Pulling Saved Items. ";
+		$saved_items = array();
+		while ($rows = mysqli_fetch_assoc($results)){
+			//array_push($saved_items, $rows);
+			$saved_items = [$rows];
+		
+		}
+		print_r($saved_items);
+		return $saved_items;
+	}else {
+		echo "No Saved Items.";
+		return false;
+	}
+	
+	}
+
+else {
+	$id = 0;
+	echo "No user found. ";
+	return false;
+}
+}
+
+
+function recommend(){
+
+$db = getDBconnect();
+$query = "SELECT strDrink, stralc from cocktails ORDER BY RAND() LIMIT 1";
+$result = mysqli_query($db, $query);
+if (mysqli_num_rows($result) > 0){
+	echo "found something ";
+	while ($row = mysqli_fetch_array($result)){
+		//array_push($resultset,$row);
+
+		return $row;
+	}
+
+
+
+	//print_r($resultset);
+	//echo "<h2> Try this on us </h2>";
+	//foreach($resultset as $item)
+	//	echo $item [0] . "<br>";
+	//	echo $item [1] . "<br>";
+	//return $resultset;
+}
+else {
+	echo "No recommendations.";
+	return false;
+}
+}
+
+
+function pullid($u,$DrinkName,$alctype,$ingredients,$instructions)
+{
+
+$db = getDBconnect();
+$sql = "SELECT id FROM Accounts WHERE username='$u'  limit 1";
+$result = mysqli_query($db,$sql);
+$num = mysqli_num_rows($result);
+
+
+
+if ($num = 1){
+	$row = mysqli_fetch_row($result);
+	$id = $row[0];
+	$recipe = "INSERT INTO CustomRep(DrinkName, AlcType, Ingredients, Instructions, id) VALUES ('$DrinkName','$alctype', '$ingredients', '$instructions', '$id')";
+	if (!mysqli_query($db,$recipe)){
+    		die('Error:' .mysqli_error());
+	}
+
+	echo "Your recipe was saved.";
+	mysqli_close($db);
+
+	return true;
+}
+else{
+	$id = 0;
+	return false;
+ 
+
+}
+}
 
 
 
@@ -21,6 +123,9 @@ function getDBconnect()
 //CREATING A NEW ACCOUNT
 function newAccount($user,$pass,$fname,$lname)
 {
+$sql = "SELECT id FROM Accounts WHERE username='$u'  limit 1";
+$result = mysqli_query($db,$sql);
+$num = mysqli_num_rows($result);
     $db = getDBconnect();    //connect 2 DB
     
     //Select by ID
@@ -36,7 +141,7 @@ function newAccount($user,$pass,$fname,$lname)
         return false;
     }
     else
-    {
+   {
          //SQL STATEMENT
         $s = "INSERT INTO Accounts (username, password, firstname, lastname) VALUES 
         ('$user', '$pass', '$fname', '$lname')";
@@ -53,7 +158,7 @@ function newAccount($user,$pass,$fname,$lname)
    
 
 
-    mysqli_close($db);
+    mysqli_close($db);	//was un-commented
 
 }
 
@@ -64,6 +169,9 @@ function showAccount($user)
    //$user = "";
     $fname = "";
    $lname = "";
+$sql = "SELECT id FROM Accounts WHERE username='$u'  limit 1";
+$result = mysqli_query($db,$sql);
+$num = mysqli_num_rows($result);
 
     //SQL STATEMENT
     $db = getDBconnect();
@@ -133,7 +241,8 @@ function doLogin($user, $pass)
     {
        
         echo 'Logging in...  ';
-        return true;
+	return true;
+
         
         
     }
@@ -144,8 +253,6 @@ function doLogin($user, $pass)
         "id" => $id,
         "text" => $text,*/
    
-
-
    
 }
 
@@ -176,7 +283,38 @@ function requestProcessor($request)
     $p = "empty";
     // Perform appropriate action depending on type
     switch ($request['type']) {
-            // Authenticate
+	    // Authenticate
+
+    case "random":
+
+	if(recommend() == false){
+		$p = "Recommend fail";
+		$rC = 6;
+		$m = "fail";
+	}else{
+		$p = "Recommend list created";
+		$rC = 7;
+		$m = recommend();
+		//array_push($m, recommend());
+	}
+	break;
+
+    case "drink":
+	    extract($request);
+	    $u=$user;
+
+	    if(pullid($u,$DrinkName,$alctype,$ingredients,$instructions) == false){
+		$p="ID pull fail";
+		$rC = 4;
+		$m = "fail";		
+	    }
+	    else
+	    {
+		    $p="ID pulled";
+		    $rC=5;
+		    $m = "success";
+	    }
+	break;	    
             case "login":
                 if(!doLogin($request['username'], $request['password']))
                 {
@@ -214,21 +352,44 @@ function requestProcessor($request)
                 break;
 
             
-            default:
-                // do nothing
-                break;
+            case "saved":
+           	extract($request);
+           	$u = $user;
+        	if (saved($u) == false){
+                	    $p = "Saved fail";
+                    	$rc = 8;
+                   	 $m = "fail";
+        	}else{
+                	$p = "Saved Items pulled";
+                	$rc = 9;
+                	$m = saved($u);
+		}
+
+		break;
+
+	    default:
+
+		break;
     }
     $response = array('returnCode'=>$rC,'message'=>$m,'payload'=>$p);
     extract($response);
-    if($returnCode > 0)
+    if($returnCode > 0 && $returnCode < 7 )
     {
-        return $returnCode;
+	    return $returnCode;
     }
-    else
-    {
-        $returnCode = 0;
-        return $returnCode;
+    elseif($returnCode == 7)
+	 {
+//    		$random = array ();
+//		array_push($random, $returnCode);
+//		array_push($random,$message);
+		return $message;
+	
     }
+    else {
+    	$returnCode = 0;
+	return $returnCode;
+    }
+
  
 }
 
@@ -238,7 +399,7 @@ function seshCheck()
     {
         $d = 3;
         echo "Please Login First -- Redirecting...";
-        header("refresh: $d; url= http://localhost/rabbitPHP/web-pages/login.html");
+        header("refresh: $d; url= http://ec2-18-224-23-159.us-east-2.compute.amazonaws.com/rabbitPHP/web-pages/login.html");
         exit();
     }
 }
@@ -253,3 +414,4 @@ function getData($o)
     return $o1;
 }
 ?>
+
